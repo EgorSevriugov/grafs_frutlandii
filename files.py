6e1b838f31a1c202e8a915cf13418564eed04949
giorgioIamthedoctor <playclevergames@mail.ru>
2016-03-20 19:06:37 +0400

    исправлено

--- files.py
+++ files.py
@@ -4,8 +4,9 @@ if len(param) > 1:
 	param[:] = param[1:]
 	for name in param:
 		files = open(name,"r")
-		txt = files.read()
-		print(txt)
+		txt = files.readlines()
+		for x in txt:
+			print(x)
 		files.close()
 else:
 	print("Too small numbers")
